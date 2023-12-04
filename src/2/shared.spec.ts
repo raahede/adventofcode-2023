23476fac22e518f814a932c60cc03fe0b51848d0
Frej Raahede Nielsen <frejraahede@gmail.com>
2023-12-04 20:15:49 +0100

    Solve 2/b

--- src/2/shared.spec.ts
+++ src/2/shared.spec.ts
@@ -1,5 +1,11 @@
 import { describe, expect, it } from 'vitest';
-import { gameConfig, gameIsPossible, solvePuzzleA } from './shared';
+import {
+  gameConfig,
+  gameIsPossible,
+  getPowerOfGame,
+  solvePuzzleA,
+  solvePuzzleB,
+} from './shared';
 
 const testData1 = `
 
@@ -46,8 +52,41 @@ describe('gameIsPossible', () => {
   });
 });
 
+describe('getPowerOfGame', () => {
+  it('returns the multipies of the highest number from each color', () => {
+    expect(
+      getPowerOfGame('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'),
+    ).toBe(48);
+    expect(
+      getPowerOfGame(
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+      ),
+    ).toBe(12);
+    expect(
+      getPowerOfGame(
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+      ),
+    ).toBe(1560);
+    expect(
+      getPowerOfGame(
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+      ),
+    ).toBe(630);
+
+    expect(
+      getPowerOfGame('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'),
+    ).toBe(36);
+  });
+});
+
 describe('solvePuzzleA', () => {
   it('does the thing...', () => {
     expect(solvePuzzleA(testData1, gameConfig)).toBe(8);
+  });
+});
+
+describe('solvePuzzleB', () => {
+  it('does the thing...', () => {
+    expect(solvePuzzleB(testData1)).toBe(2286);
   });
 });
--- src/2/shared.ts
+++ src/2/shared.ts
@@ -48,10 +48,51 @@ export const gameIsPossible = (input: string, config: GameConfig): boolean => {
   return isPossible;
 };
 
+export const getPowerOfGame = (input: string): number => {
+  const powers = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  input
+    .substring(input.indexOf(':') + 1)
+    .split(';')
+    .forEach((part) =>
+      part.split(',').forEach((partItem) => {
+        // '3 blue'
+        // '4 red'
+        // '2 green'
+        const str = partItem.trim();
+        const number = parseInt(str.substring(0, str.indexOf(' ')));
+        const color = str.substring(str.indexOf(' ') + 1) as
+          | 'blue'
+          | 'red'
+          | 'green';
+
+        if (!Object.keys(powers).includes(color))
+          throw new Error(`Color "${color}" not found in powers map`);
+
+        if (number > powers[color]) powers[color] = number;
+      }),
+    );
+
+  return powers.blue * powers.green * powers.red;
+};
+
 export const solvePuzzleA = (input: string, config: GameConfig): number => {
   const rows = getRowsFromTextBlock(input);
   const rowResults = rows.flatMap((gameStr) => {
     return gameIsPossible(gameStr, config) ? getGameId(gameStr) : [];
+  });
+
+  return getSum(rowResults);
+};
+
+export const solvePuzzleB = (input: string): number => {
+  const rows = getRowsFromTextBlock(input);
+  const rowResults = rows.map((gameStr) => {
+    return getPowerOfGame(gameStr);
   });
 
   return getSum(rowResults);
