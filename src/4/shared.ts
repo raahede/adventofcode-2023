130bcad5c25e4eb6e71eaf0143f5bd2c29a1e3e5
Frej Raahede Nielsen <frejraahede@gmail.com>
2023-12-05 21:16:24 +0100

    Refactor to prepare for b

--- src/4/shared.ts
+++ src/4/shared.ts
@@ -17,19 +17,26 @@ export const getCardNumbers = (input: string): number[][] => {
   return [parseNumberString(winningNumbersStr), parseNumberString(numbersStr)];
 };
 
-export const getCardPoints = (input: string): number => {
+export const getCardId = (input: string): number => {
+  const idStr = input.match(/(?<=Card \s*).*?(?=\s*:)/gs);
+  if (!idStr) throw new Error('Could not find ID');
+  return parseInt(idStr[0]);
+};
+
+export const getCardInfo = (input: string): number[] => {
+  const id = getCardId(input);
   const [winningNumbers, numbers] = getCardNumbers(input);
   const matchingNumbers = numbers.filter((num) => winningNumbers.includes(num));
-
   // Starting with 1 point, multiply by 2 for each matching number
-  return matchingNumbers.reduce(
+  const points = matchingNumbers.reduce(
     (partialSum) => (partialSum === 0 ? 1 : partialSum * 2),
     0,
   );
+  return [id, points];
 };
 
 export const solvePuzzleA = (input: string): number => {
   const rows = getRowsFromTextBlock(input);
-  const rowResults = rows.map((row) => getCardPoints(row));
+  const rowResults = rows.map((row) => getCardInfo(row)[1]);
   return getSum(rowResults);
 };
